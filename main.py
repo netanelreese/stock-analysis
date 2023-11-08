aff4c2eaf7c8df21ec1e85f4bfc0a6d90730de26
Nathanael <ngreese7@ou.edu>
2023-11-08 11:38:05 -0600

    Candlestick  Graph

--- main.py
+++ main.py
@@ -22,3 +22,9 @@ data = data[["Date", "Open", "High", "Low",
              "Close", "Adj Close", "Volume"]]
 data.reset_index(drop=True, inplace=True)
 print(data.head())
+
+figure = go.Figure(data=[go.Candlestick(x=data["Date"],
+                                        open=data["Open"], high=data["High"],
+                                        low=data["Low"], close=data["Close"])])
+figure.update_layout(title = "Google Stock Price Analysis", xaxis_rangeslider_visible=False)
+figure.show()
